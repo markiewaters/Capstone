d819515c238501ba2d0f83785920cf9b638bf4c4
markiewaters <darkmark53@gmail.com>
2023-10-20 14:03:02 -0400

    TA guidance

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,26 +1,14 @@
 import html from "html-literal";
-import travelImg from "/assets/img/walkImg.png";
 
 export default (state) => html`
   <section class="Homepage">
       <h2 class="introDesc">
         Find your next journey here!
       </h2>
-      <div class="headerImg">
-        <img class="heroImg" src="${travelImg}" />
-      </div>
-      <div class="twoColumns">
-        <div class="trackingList">
-          Tracking List will go here(2 Column Section)
-        </div>
-        <div class="mapQuest">MapQuest will go here (2 Column Section)</div>
+      <div class="mapContainer">
+        <div id="map"></div>
       </div>
     </div>
-    <h3>
-      The weather in ${state.weather.city} is ${state.weather.description}.
-      Temperature is ${state.weather.temp}F, and it feels like
-      ${state.weather.feelsLike}F.
-    </h3>
   </section>
 
 `;
--- index.js
+++ index.js
@@ -22,6 +22,96 @@ function afterRender() {
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
+
+  // if (state.view === "Home") {
+  //   /*
+  //     Please refer to the documentation:
+  //     https://developer.mapquest.com/documentation/mapquest-js/v1.3/
+  //   */
+
+  //   L.mapquest.key = "KEY";
+
+  //   // 'map' refers to a <div> element with the ID map
+  //   L.mapquest.map("map", {
+  //     center: [37.7749, -122.4194],
+  //     layers: L.mapquest.tileLayer("map"),
+  //     zoom: 12,
+  //   });
+
+  //   L.mapquest.key = process.env.MAPQUEST_API_KEY;
+
+  //   const baseLayer = L.mapquest.tileLayer("map");
+  //   const precipitationLayer = L.tileLayer(
+  //     `https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`,
+  //     { layer: "precipitation_new" }
+  //   );
+  //   const temperatureLayer = L.tileLayer(
+  //     `https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`,
+  //     { layer: "temp_new" }
+  //   );
+  //   const windLayer = L.tileLayer(
+  //     `https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`,
+  //     { layer: "wind_new" }
+  //   );
+
+  //   // 'map' refers to a <div> element with the ID map
+  //   const map = L.mapquest.map("map", {
+  //     center: [42, -71],
+  //     layers: baseLayer,
+  //     zoom: 5,
+  //   });
+
+  //   L.mapquest
+  //     .textMarker([42, -71], {
+  //       text: "Sample Marker",
+  //       subtext: "Click Here for More Details",
+  //       position: "right",
+  //       type: "marker",
+  //       hover: "Howdy",
+  //       icon: {
+  //         primaryColor: "#333333",
+  //         secondaryColor: "#333333",
+  //         size: "sm",
+  //       },
+  //     })
+  //     .addTo(map);
+
+  //   L.marker([30, -90], {
+  //     icon: L.mapquest.icons.marker({
+  //       primaryColor: "#22407F",
+  //       secondaryColor: "#3B5998",
+  //       shadow: true,
+  //       size: "md",
+  //       // symbol: 'T'
+  //     }),
+  //   }).addTo(map);
+
+  //   map.addControl(L.mapquest.control());
+
+  //   L.mapquest
+  //     .directionsControl({
+  //       routeSummary: {
+  //         enabled: false,
+  //       },
+  //       narrativeControl: {
+  //         enabled: true,
+  //         compactResults: false,
+  //       },
+  //     })
+  //     .addTo(map);
+
+  //   // https://leafletjs.com/reference.html#control-layers
+  //   L.control
+  //     .layers(
+  //       {},
+  //       {
+  //         Temperature: temperatureLayer,
+  //         Precipitation: precipitationLayer,
+  //         Wind: windLayer,
+  //       }
+  //     )
+  //     .addTo(map);
+  // }
 }
 
 router.hooks({
